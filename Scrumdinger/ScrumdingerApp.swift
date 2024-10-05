32c9da68e0a64596a6f05beb775b974183036c0a
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2024-10-04 20:43:02 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tamilore Oladejo on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
