f3a020b29dab80e3f73b3edd1afaf9bd937d9a44
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2024-10-05 13:47:05 -0500

    Views - Creating a card view
    - completed section 4 and 5

--- Scrumdinger/Models/TrailingIconLabelStyle.swift
+++ Scrumdinger/Models/TrailingIconLabelStyle.swift
@@ -5,4 +5,19 @@
 //  Created by Tamilore Oladejo on 10/5/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+    
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
