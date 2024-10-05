a201278d307225ff87868730866be8d7ee48cf2c
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2024-10-05 13:03:41 -0500

    Views - Creating a card view
    - completed section 3

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Tamilore Oladejo on 10/5/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
